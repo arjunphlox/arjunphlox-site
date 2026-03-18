018b8ae9c87e0c808a94d0ede655d000f3d4a500
Arjun Phlox <arjunphlox@gmail.com>
2026-03-18 23:52:16 +0530

    add astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://arjunphlox.github.io/arjunphlox-site',
+    base: '/arjunphlox-site',
+      output: 'static',
+      });
+      
